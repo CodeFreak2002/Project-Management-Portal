2d904aba1dd737391ab610bcf067c4aab03db276
CodeFreak2002 <ashwinarun2002@gmail.com>
2023-02-14 18:07:59 +0530

    Made database url private

--- server/server.js
+++ server/server.js
@@ -4,14 +4,16 @@ const port = 8080;
 const logger = require("morgan");
 const bodyParser = require("body-parser");
 var util = require('util');
+require('dotenv').config()
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended : true}));
 app.use(logger("dev"));
 var encoder = new util.TextEncoder('utf-8');
 
-const dbURL = 'mongodb+srv://team03:team03_mongo@cluster0.oaiv4hh.mongodb.net/Project-Management?retryWrites=true&w=majority';
+const dbURL = process.env.DATABASE_URL;
 const mongoose = require("mongoose");
+
 mongoose.connect(dbURL , {useNewUrlParser: true , useUnifiedTopology : true})
     .then((result) => {
         app.listen(port , function() {
