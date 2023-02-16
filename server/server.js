179ad273308d2d74cd1185df66e01c79115ba84c
CodeFreak2002 <ashwinarun2002@gmail.com>
2023-02-16 19:13:48 +0530

    Added student registration

--- server/server.js
+++ server/server.js
@@ -3,16 +3,23 @@ const app = express();
 const port = 8080;
 const logger = require("morgan");
 const bodyParser = require("body-parser");
+
 const teacher = require("./routes/teacher.js");
 const classes = require("./routes/class.js");
+const student = require("./routes/student.js");
+
 var util = require('util');
 require('dotenv').config()
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended : true}));
+
 app.use(logger("dev"));
+
 app.use("/teacher" , teacher);
 app.use("/class" , classes);
+app.use("/student", student)
+
 var encoder = new util.TextEncoder('utf-8');
 
 const dbURL = process.env.DATABASE_URL;
