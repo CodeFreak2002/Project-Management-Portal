e098a781ec0439ae904eaee42c5568067f9160fc
Ashwin-1709 <ashwinpugalia1@gmail.com>
2023-02-16 20:34:21 +0530

    student login done

--- server/routes/student.js
+++ server/routes/student.js
@@ -3,16 +3,11 @@ const router = express.Router();
 const Student = require('../models/student');
 
 router.post('/register', async function(req,res){
-    
     var student = await Student.findOne({email : req.body.email}).clone();
-
     if (student)
         return res.status(500).send("Email already registered");
-    
-    if (req.body.phone)
-    {
+    if (req.body.phone) {
         var student = await Student.findOne({phone : req.body.phone}).clone();
-
         if (student)
             return res.status(500).send("Phone already registered");
     }
@@ -26,6 +21,16 @@ router.post('/register', async function(req,res){
 
     await newStudent.save();
     return res.status(200).send("Registration successful");
+});
+
+router.post('/login' , async function(req , res) {
+    let student = await Student.findOne({email : req.body.email}).clone();
+    if(student) {
+        if(student.password === req.body.password)
+            return res.status(200).send(student);
+        return res.status(500).send("Invalid Password");
+    } 
+    return res.status(500).send("Invalid Credentials");
 });
 
 module.exports = router
