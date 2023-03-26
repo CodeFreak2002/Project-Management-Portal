bdd337bf799be04f6956199bef222c9832745846
SriramB2002 <f20200002@hyderabad.bits-pilani.ac.in>
2023-03-27 00:10:24 +0530

    Error handling in register done

--- src/Register.jsx
+++ src/Register.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import {
   MDBBtn,
   MDBContainer,
@@ -14,8 +14,9 @@ import {
 from 'mdb-react-ui-kit';
 import './Register.css';
 import axios from 'axios';
-import { MenuItem, Select, Button } from '@mui/material';
+import { MenuItem, Select, Button, Snackbar, Alert } from '@mui/material';
 import background from './StudentDashboard/background.png'
+import { Link } from 'react-router-dom';
 
 function Register() {
 
@@ -24,19 +25,48 @@ function Register() {
   const [password, setPassword] = useState("");
   const [phone, setPhone] = useState("");
   const [role, setRole] = useState("Student");
+  const [open, setOpen] = useState(false);
+  const [severity, setSeverity] = useState("");
+  const [msg, setMsg] = useState("");
+
+  useEffect(() => {}, [open, severity, msg]);
+
+  const handleClose = (event, reason) => {
+    if (reason === 'clickaway') {
+      return;
+    }
+    setOpen(false);
+    setSeverity("");
+    setMsg("");
+  };
 
   const handleSubmit = async () => {
-    console.log(name, email, password, phone);
-    if (role === "Student") {
+    if (name.length === 0 || email.length === 0 || password.length === 0 || phone.length === 0) {
+      setOpen(true);
+      setSeverity("error");
+      setMsg("Please fill out all fields!");
+    }
+    else if (role === "Student") {
       await axios.post('http://project-management-portal-server.vercel.app/student/register', {
         name: name,
         email: email,
         password: password,
         phone: phone
       }).then((res) => {
-        console.log(res);
+        if (res.status === 200) {
+          setOpen(true);
+          setSeverity("success");
+          setMsg("Successfully registered!");
+        }
+        else {
+          setOpen(true);
+          setSeverity("warning");
+          setMsg(res.data);
+        }
       }).catch((err) => {
-        console.log(err);
+        setOpen(true);
+        setSeverity("error");
+        setMsg(err.response.data);
       })
     }
     else if (role === "Teacher") {
@@ -46,26 +76,45 @@ function Register() {
         password: password,
         phone: phone
       }).then((res) => {
-        console.log(res);
+        if (res.status === 200) {
+          setOpen(true);
+          setSeverity("success");
+          setMsg("Successfully registered!");
+        }
+        else {
+          setOpen(true);
+          setSeverity("warning");
+          setMsg(res.data);
+        }
       }).catch((err) => {
-        console.log(err);
+        setOpen(true);
+        setSeverity("error");
+        setMsg(err.response.data);
       })
     }
   }
 
   return (
-    <MDBContainer fluid className=' register d-flex align-items-center justify-content-center bg-image' style={{backgroundImage: `url(${background})`}}>
+    <MDBContainer fluid className=' register d-flex align-items-center justify-content-center bg-image' style={{backgroundImage: `url(${background})`, textAlign: 'center'}}>
       <div className='mask gradient-custom-3'></div>
       <MDBCard className='m-5' style={{maxWidth: '600px'}}>
         <MDBCardBody className='px-5'>
-          <h2 className="text-center mb-5">Create an account</h2>
-          <MDBInput wrapperClass='mb-4' label='Your Name' size='lg' id='form1' type='text'/>
-          <MDBInput wrapperClass='mb-4' label='Your Email' size='lg' id='form2' type='email'/>
-          <MDBInput wrapperClass='mb-4' label='Password' size='lg' id='form3' type='password'/>
-          <MDBInput wrapperClass='mb-4' label='Phone' size='lg' id='form4' type='password'/>
-          <MDBBtn className='mb-4 w-100 gradient-custom-4' size='lg'>Register</MDBBtn>
+          <h2 className="text-center mb-4">Create an account</h2>
+          <MDBInput wrapperClass='mb-4' label='Your Name' size='lg' id='form1' type='text' value={name} onChange={(e) => setName(e.target.value)}/>
+          <MDBInput wrapperClass='mb-4' label='Your Email' size='lg' id='form2' type='email' value={email} onChange={(e) => setEmail(e.target.value)}/>
+          <MDBInput wrapperClass='mb-4' label='Password' size='lg' id='form3' type='password' value={password} onChange={(e) => setPassword(e.target.value)}/>
+          <MDBInput wrapperClass='mb-4' label='Phone' size='lg' id='form4' type='tel' value={phone} onChange={(e) => setPhone(e.target.value)}/>
+          <Select style={{marginBottom: '5%', textAlign: 'center'}} size="small" value={role} fullWidth onChange={(e) => setRole(e.target.value)}>
+            <MenuItem value={"Student"}>Student</MenuItem>
+            <MenuItem value={"Teacher"}>Teacher</MenuItem>
+          </Select>
+          <Button variant='contained' style={{marginBottom: '3%'}} fullWidth onClick={handleSubmit}>Register</Button>
+          <Link to="/login">Already registered? Sign in here.</Link>
         </MDBCardBody>
       </MDBCard>
+      <Snackbar open={open} autoHideDuration={5000} onClose={handleClose}>
+        <Alert severity={severity} variant="filled" onClose={handleClose}>{msg}</Alert>
+      </Snackbar>
     </MDBContainer>
   );
 }
