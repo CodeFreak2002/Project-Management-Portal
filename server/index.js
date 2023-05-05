203fb69235af594b806c93e0a72f08efa1c47787
Ashwin-1709 <ashwinpugalia1@gmail.com>
2023-05-05 10:36:13 +0530

    security hotspot fix

--- server/index.js
+++ server/index.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const app = express();
+app.disable("x-powered-by");
 const port = 5000;
 const logger = require("morgan");
 const bodyParser = require("body-parser");
